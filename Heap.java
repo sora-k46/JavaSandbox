b34eaea5a3d3a20797a3dec7128e7f1be1efb1e3
Sorrawit <sorrawit456789@gmail.com>
2022-04-27 10:56:41 +0700

    create array

--- Heap.java
+++ Heap.java
@@ -1,2 +1,8 @@
 public class Heap {
+  public static void main(String[] args) {
+    int[] data = {44, 55, 2, 77, 29, 23, 300, 52, 76, 22, 19, 3, 100};
+  }
+
+  public static void heapSort(int[] ar) {
+  }
 }
