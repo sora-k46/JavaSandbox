385e15b5832418b41c90fe6532e0b694ea58a12b
Sorrawit <sorrawit456789@gmail.com>
2022-04-27 10:59:13 +0700

    change println to print

--- Heap.java
+++ Heap.java
@@ -8,7 +8,7 @@ public class Heap {
 
   public static void print(int[] arr) {
     for (int i = 0; i < arr.length; i++) {
-      System.out.println(arr[i]);
+      System.out.print(arr[i] + " ");
     }
     System.out.println();
   }
