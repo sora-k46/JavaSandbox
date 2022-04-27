94ffb5253fd6a921d5a43250544c68e2eacd7359
Sorrawit <sorrawit456789@gmail.com>
2022-04-27 10:57:22 +0700

    create print for array method

--- Heap.java
+++ Heap.java
@@ -3,6 +3,9 @@ public class Heap {
     int[] data = {44, 55, 2, 77, 29, 23, 300, 52, 76, 22, 19, 3, 100};
   }
 
-  public static void heapSort(int[] ar) {
+  public static void heapSort(int[] arr) {
+  }
+
+  public static void print(int[] arr) {
   }
 }
