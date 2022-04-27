d278c7fb20bfa0432faff184371203e466f66f0b
Sorrawit <sorrawit456789@gmail.com>
2022-04-27 11:33:31 +0700

    remove heapify method and format code

--- Heap.java
+++ Heap.java
@@ -1,6 +1,6 @@
 public class Heap {
   public static void main(String[] args) {
-    int[] data = {44, 55, 2, 77, 29, 23, 300, 52, 76, 22, 19, 3, 100};
+    int[] data = { 44, 55, 2, 77, 29, 23, 300, 52, 76, 22, 19, 3, 100 };
   }
 
   public static void heapSort(int[] arr) {
@@ -8,10 +8,7 @@ public class Heap {
       swap(arr, i, 0);
       moveDown(arr, 0, i);
     }
-  }
-
-  public static void heapify(int[] arr) {
-    for (int i = (arr.length/2) - 1; i >= 0; i--) {
+    for (int i = (arr.length / 2) - 1; i >= 0; i--) {
       moveDown(arr, i, arr.length);
     }
   }
