3d37782a6c5cf25d83adbc83e9ab01b61a59d06f
Sorrawit <sorrawit456789@gmail.com>
2022-04-29 17:56:44 +0700

    Create quickSort method

--- QuickSort.java
+++ QuickSort.java
@@ -2,4 +2,8 @@ public class QuickSort {
   public static void main(String[] args) {
     int[] data = { 69, 200, 44, 1000, 3, 256, 400, 500, 24, 55, 222, 8, 10, 111, 123, 1, 205 };
   }
+
+  public static void quickSort(int[] a, int first, int last) {
+
+  }
 }
